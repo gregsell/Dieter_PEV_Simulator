58908e36f26b767eb3d0ca7756d7bd4bf34a81cb
Gregor Sellschopp <gregor.sellschopp@gmx.de>
2026-05-22 09:32:12 +0200

    refractor: details + method comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,15 @@ description of protocol DieterEvDriver <-> Dieter:
 
 commands (host -> MCU)
   keys                   possible values            
-  set_contactor             {0,1}                            
-  set_connector_lock        {0,1}   
-  set_state_c               {0,1}
+  set_contactor             {0, 1}                            
+  set_connector_lock        {0, 1}   
+  set_state_c               {0, 1}
 
 measurements (MCU -> host)
-  u_inlet                   {0,1,2...}
-  cp_duty_cycle             {0,1,..99,100}
-  connector_lock_confirmed  {0,1}
+  u_inlet                   {0, 1, 2...}
+  current                   {0.0, 1.0, 2.0,..}
+  cp_duty_cycle             {0, 1, ..99, 100}
+  connector_lock_confirmed  {0, 1}
 
 each newline-terminated and separated by colon.
 
@@ -34,8 +35,9 @@ examples:
 # define PIN_CONNECTOR_LOCK_FB 9
 
 # define PIN_POWER_RELAY_OUTPUT 6
+# define PIN_POWER_RELAY_FB 3
 # define PIN_CP_MOSFET_OUTPUT 8
-// 3 contactor FB
+
 # define PIN_MOTOR_DRIVER_A 4
 # define PIN_MOTOR_DRIVER_B 5
 // chip select for SPI comm with ADS
@@ -44,11 +46,13 @@ examples:
 ADS1118 ads1118(PIN_CS_ADS1118);
 
 
+const unsigned long PUBLISH_INTERVAL_MS = 500;
 
-const unsigned long LOCK_TIMEOUT_MS = 600; // time-based control (not current)
+const unsigned long LOCK_TIMEOUT_MS = 600; // time-based control (not current) of connector lock motor
+
 const int N_AVERAGE = 10;   // averaging over several read input values
-const int LINE_MAX = 42;    // top limit for msg length
 
+const int LINE_MAX = 42;    // top limit for UART msg length
 char in_line[LINE_MAX];     // input line buffer
 int in_len = 0;             // current length of buffer
 
@@ -61,7 +65,7 @@ enum class LockState {
 // keep track of what lock motor is doing
 LockState lock_state = LockState::IDLE;
 
-unsigned long lasttime_500ms = 0;
+unsigned long t_last_publish = 0;
 unsigned long lock_timer_start = 0;
 
 int u_inlet;                // DC voltage measured at HV connect, before relays
@@ -69,6 +73,7 @@ float current;              // DC current on HV lines
 int cp_duty_cycle;          // duty cycle of C_p line
 int connector_lock_confirmed = 0;// feedback switch status from CCS connector lock
 
+// read CCS inlet voltage by reading values from external ADC via SPI
 void readInletVoltage() {
   float tmp = 0;
   // averaging ADC readings
@@ -87,6 +92,7 @@ void readCurrent() {
   current = raw * 5.0 / 1023 * CURR_SENSE_R12 / (CURR_SENSE_R12 + CURR_SENSE_R14);
 }
 
+// measure PWM duty cycle of CP line 
 void readCpDutyCycle() {
   // three cases to distinguish: logic high, logic low, or 1 kHz PWM
   unsigned long t = pulseIn(PIN_CP_DUTY_CYCLE_INPUT, HIGH, 3000); // use pulseIn timeout of 3 ms to rule out PWM
@@ -101,7 +107,7 @@ void readCpDutyCycle() {
     int counter = N_AVERAGE;
     for (int i = 0; i < counter; i++) {
       int raw = pulseIn(PIN_CP_DUTY_CYCLE_INPUT, HIGH, 2000);
-      if (raw == 0) counter--; // in case of timeout the reading is omitted
+      if (raw == 0) counter--; // in case of timeout the reading is omitted       <------- REMOVE?
       tmp += raw;
     }
     if (tmp == 0) { // If the sum ist still zero, there is an error
@@ -114,12 +120,14 @@ void readCpDutyCycle() {
   }
 }
 
+// read in the state of the microswitch inside the CCS inlet locking actuator 
 void readConnLockFB() {
   // internal pullup resistor
   // for now assume low-side closing switch
   //connector_lock_confirmed = !digitalRead(PIN_CONNECTOR_LOCK_FB);
 }
 
+// publish all measurements via UART
 void publishMeasurements() {
   // schema:
   // key:value\n
@@ -136,6 +144,7 @@ void publishMeasurements() {
   Serial.println(s);
 }
 
+// process single received line 
 void process_line(const char *line) {
   char* separator = strchr(line, ':');
   if (separator == nullptr) {
@@ -149,6 +158,7 @@ void process_line(const char *line) {
   key[keyLen] = '\0'; // zero termination
 
   char* value = separator + 1;
+  // Serial.print("found key: "); Serial.print(key); Serial.print(" and value: "); Serial.println(value);
 
   if (strcmp(key, "set_contactor") == 0) {
     int cmd = atoi(value);
@@ -156,7 +166,6 @@ void process_line(const char *line) {
   } 
   else if (strcmp(key, "set_connector_lock") == 0) {
     int cmd = atoi(value);
-    Serial.print("Dieter cmd: "); Serial.println(cmd);
     if (cmd == 1 || cmd == 0) {
       // drive differential pins of L298N input
       // (enable of L298N is pulled high => always max speed)
@@ -173,10 +182,12 @@ void process_line(const char *line) {
     int cmd = atoi(value);
     if (cmd == 1 || cmd == 0) digitalWrite(PIN_CP_MOSFET_OUTPUT, cmd);
     }
-else Serial.println("unknown command");
+  else Serial.println("unknown command");
   // else unknown command
 }
 
+// the CCS connector lock actuator does not lock instantly.
+// This method is called periodically to check if a change of control signals is necessary
 void connector_lock_task() {
   if (millis() - lock_timer_start >= LOCK_TIMEOUT_MS) {
     // set both inputs equal, this stops motor (see L298 datasheet)
@@ -187,6 +198,7 @@ void connector_lock_task() {
   }
 }
 
+// periodic check of serial interface
 void serial_rx_task() {
   while (Serial.available() > 0) {
     char c = (char)Serial.read();
@@ -211,7 +223,8 @@ void serial_rx_task() {
 void dummyValues(){
   u_inlet = 400;
   cp_duty_cycle = 5;
-  //connector_lock_confirmed = 1;
+  connector_lock_confirmed = 1;
+  current = 42;
 }
 
 void setup() {
@@ -219,7 +232,7 @@ void setup() {
   pinMode(PIN_CP_MOSFET_OUTPUT, OUTPUT);
   pinMode(PIN_MOTOR_DRIVER_A, OUTPUT);
   pinMode(PIN_MOTOR_DRIVER_B, OUTPUT);
-  
+
   digitalWrite(PIN_POWER_RELAY_OUTPUT, 0);
   digitalWrite(PIN_CP_MOSFET_OUTPUT,0);
 
@@ -229,7 +242,7 @@ void setup() {
   analogReference(INTERNAL);
 
   Serial.begin(19200);
-  
+
   /* Changing the sampling rate. RATE_8SPS, RATE_16SPS, RATE_32SPS, RATE_64SPS, RATE_128SPS, RATE_250SPS, RATE_475SPS, RATE_860SPS*/
   ads1118.setSamplingRate(ads1118.RATE_16SPS);
   /* Changing the input selected. Differential inputs: DIFF_0_1, DIFF_0_3, DIFF_1_3, DIFF_2_3. Single ended input: AIN_0, AIN_1, AIN_2, AIN_3*/
@@ -242,16 +255,19 @@ void setup() {
 }
 
 void loop() {
-  
+
+  readInletVoltage();
   readCurrent();
   readCpDutyCycle();
   readConnLockFB();
 
+  //dummyValues();
+  
   serial_rx_task();
   connector_lock_task();
-
-  if ((millis() - lasttime_500ms)>500) {
-    lasttime_500ms = millis();
+  
+  if ((millis() - t_last_publish)>PUBLISH_INTERVAL_MS) {
+    t_last_publish = millis();
     publishMeasurements();
   }
 }
